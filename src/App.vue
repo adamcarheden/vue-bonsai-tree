0fcb01f5a218c479f4c9d7e07a8432e8d091e80b
Adam Carheden <adam.carheden@gmail.com>
2016-10-13 00:43:32 -0600

    Fully-featured, manually tested

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,76 @@
 <template>
   <div id="app">
+
+  <!-- A simple root element: It's content becomes the item text, except for the 'content' slot, which appears below and can be shown or hidden -->
+  <bonsai initialState='expanded'>
+    Tree Root
+
+    <!-- The 'content' slot can contain other <bonsai> elements, creating a tree -->
+    <div slot='content'>
+
+      <!-- We can define content as an attribute instead of a slot -->
+      <bonsai content='Quick, hide me! Hide me!'>
+        It's the cops
+      </bonsai>
+
+      <!-- Slots take precedence over attributes -->
+      <bonsai content="You'll never see me">
+        Do you see me?
+        <span slot='content'>I see you</span>
+      </bonsai>
+
+      <!-- We can define the item text as an attribute too -->
+      <bonsai item='Look at me'>
+        <div slot='content'>
+          <!-- But any text in the body constitutes a slot for the item, so that slot still takes precedence over the attribute -->
+          <bonsai item='The invisible man'>
+            Keven Bacon
+          </bonsai>
+        </div>
+      </bonsai>
+
+      <!-- We can even customize the expand/collapse buttons -->
+      <bonsai>
+        Click me
+        <span slot='expand'>&larr;</span>
+        <span slot='collapse'>&rarr;</span>
+        <div slot='content'>
+          <!-- Attributes work for that as well -->
+          <bonsai expand="&" collapse='|'>
+            Me Too
+            <span slot='content'>
+              <!-- You can, of course, mix and match -->
+              <bonsai item='Deep' collapse="^" >
+                <span slot='content'>Throat</span>
+                <!-- That's handy, because Vue escapes HTML entities (like &Darr; below) that it finds in attirbutes -->
+                <span slot='expand'>&Darr;</span>
+              </bonsai>
+            </span> 
+          </bonsai>
+        </div>
+      </bonsai>
+
+      <!-- Here's what you get with all the defaults -->
+      <bonsai/>
+
+    </div>
+  </bonsai>
+
+  <hr/>
     <bonsai>
       Root node a={{ obj.a }}
-      <div slot='children'>
+      <div slot='content'>
 
         <bonsai>
           First Child
-          <div slot='children'>
+          <div slot='content'>
             <bonsai>I'm a leaf {{obj.b}}</bonsai>
           </div>
         </bonsai>
 
         <bonsai>
           Second Child
-          <div slot='children'>grandkid {{obj.c}} {{obj.d}}</div>
+          <div slot='content'>grandkid {{obj.c}} {{obj.d}}</div>
         </bonsai>
 
       </div>
--- src/components/bonsai.vue
+++ src/components/bonsai.vue
@@ -2,33 +2,68 @@
   <div v-bind:class="{ expanded: expanded }">
 
     <div class='item' @click="expanded = (expanded ? false : true)">
-      <div v-if="leaf()" class='expand-btn'>
-        <span v-if="expanded">-</span>
-        <span v-else>+</span>
+      <div v-if="isLeaf" class='expand-btn'>
+        <span v-if="expanded" class='collapse'><slot name='collapse'>{{ collapse }}</slot></span>
+        <span v-else          class='expand'>  <slot name='expand'>  {{ expand }}  </slot></span>
       </div>
-      <div class='item-content'><slot></slot></div>
+      <div class='item-content'>
+        <span v-if="hasItem"><slot>{{ item }}</slot></span>
+        <span v-else>{{ item }}</span>
+      </div>
     </div>
 
-    <div class='children' v-if="expanded && leaf()"><slot name='children'>&nbsp;</slot></div>
+    <div class='content' v-if="expanded && isLeaf"><slot name='content'>{{ content }}</slot></div>
 
   </div>
 </template>
 <script>
 export default {
-  params: ['obj'],
+  props: {
+    item: {
+      type: String,
+      default: 'Bonsai!'
+    },
+    expand: {
+      type: String,
+      default: '+'
+    },
+    collapse: {
+      type: String,
+      default: '-'
+    },
+    content: {
+      type: String,
+      default: ''
+    },
+    initialState: {
+      type: String,
+      default: 'collapsed',
+      validator: function (value) {
+        return ['expanded', 'collapsed'].indexOf(value.toLowerCase()) >= 0
+      }
+    }
+  },
   data: function () {
     return {
       expanded: false
     }
   },
-  methods: {
-    leaf: function () {
-      return ('children' in this.$slots)
+  created: function () {
+    this.expanded = this.initialState === 'expanded'
+  },
+  computed: {
+    hasItem: function () {
+      // Elements with no body (<bonsai/> and <bonsai></bonsai>) result in slots being empty
+      // That's an inconcistency in the Vue API. The left-hand site of the || is the workaround
+      return (!('default' in this.$slots)) || this.$slots.default[0].text.trim() !== ''
+    },
+    isLeaf: function () {
+      return ('content' in this.$slots) || this.content !== ''
     }
   }
 }
 </script>
-<style>
+<style scoped>
 .item {
   display: block;
 }
@@ -38,7 +73,7 @@ export default {
 .item-content {
   padding-left: .2em;
 }
-.children {
+.content {
   display: block;
   margin-left: 0.5em;
 }
